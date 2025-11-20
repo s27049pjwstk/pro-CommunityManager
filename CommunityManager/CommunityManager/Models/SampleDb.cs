485d175c718a350686b62988e61ecbde128bfe57
Puotek <hubertosz.p@gmail.com>
2025-11-20 04:37:43 +0100

    Database created

---
+++ CommunityManager/CommunityManager/Models/SampleDb.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CommunityManager.Models;
+
+public class SampleDb : DbContext {
+    public DbSet<WorkOrder> WorkOrders => Set<WorkOrder>();
+
+    public SampleDb() {}
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) {
+        optionsBuilder.UseSqlite("Data Source=database.db");
+    }
+}
+
+public class WorkOrder {
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public DateTime Workdate { get; set; }
+    public int Somevalue { get; set; }
+}
